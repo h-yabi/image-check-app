32e7e6127daa12d80e2c605302bf1bc4f26a0df8
h_yabiku <>
2022-06-26 12:58:21 +0900

    IconPlus コンポーネント作成

---
+++ components/atoms/icons/IconPlus/IconPlus.stories.js
@@ -0,0 +1,12 @@
+import IconPlus from './IconPlus'
+
+export default {
+  component: IconPlus,
+  title: 'atoms/icons/IconPlus',
+}
+
+export const Default = () => ({
+  components: { IconPlus },
+  template: '<IconPlus />',
+})
+Default.storyName = 'デフォルト'
